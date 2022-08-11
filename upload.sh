f5d86cebcbeb2b7c70ecb17a7e83cf1acaf02c31
PacoReinaCampo <frareicam@gmail.com>
2022-08-11 09:28:22 +0200

    Working in MPSoC-MSP430 🔨

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git config --global user.email "frareicam@gmail.com"
 git config --global user.name "PacoReinaCampo"
 git add *
-git commit -m "Working in MPSoC-MSP430"
+git commit -m "Working in MPSoC-MSP430 🔨"
 git push origin master
