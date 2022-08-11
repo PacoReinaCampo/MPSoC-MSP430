0e51bc2ccbf11019c6e63707e82dd6a619f882db
PacoReinaCampo <frareicam@gmail.com>
2022-08-11 10:37:40 +0200

    Working in MPSoC-MSP430 🔨

--- doc/write.sh
+++ doc/write.sh
@@ -1,8 +1,5 @@
 rm -f *.tex
 rm -f *.pdf
 
-pandoc ../README.md -s -o MPSoC-MSP430.tex
-pandoc ../README.md -s -o MPSoC-MSP430.pdf
-
-pandoc ../README-ES.md -s -o MPSoC-MSP430-ES.tex
-pandoc ../README-ES.md -s -o MPSoC-MSP430-ES.pdf
+pandoc BOOK.md -s -o MPSoC-MSP430.tex
+pandoc BOOK.md -s -o MPSoC-MSP430.pdf
